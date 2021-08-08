9134a2b253ce930081c4658749efe3014b9a1276
Siddhesh <bhosalesiddhesh9@gmail.com>
2021-08-08 09:51:11 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import style from './style.css'
+import { useState } from "react";
+import Booksdata from './Booksdata';
+function App(){
+  const [type,settype] = useState("PersonalGrowth");
+ 
+  return(
+    <div className="container">
+        <h1 className="title">Siddhesh Book's recommendation</h1>
+        <h2 className="info">Click the following option as per your interest</h2>
+        <div className="buttons">
+          {/* <button onClick={(e)=>{settype("Finance")}}>Finance</button>
+          <button onClick={(e)=>{settype("sport")}}>sports</button>
+          <button onClick={(e)=>{settype("javascript")}}>javascript</button>
+          <button onClick={(e)=>{settype(Data structure)}}>Data structure</button> */}
+
+          {Object.keys(Booksdata).map((value)=>(
+          <button className="button" onClick={()=>(settype(value))}>{value}</button>
+          ))}
+        </div>
+
+        <div className="types">{type} 👇</div>
+        <div className="download">(Click on book Image to download it's softcopy)</div>
+
+        <div className="bookscontainer">
+        {Booksdata[type].map((book) => (
+          <div className="bookItem">
+            <a href={book.softcopy}><img
+              className="bookImage"
+              src={book.photoURL}
+              alt={book.bookName}
+            ></img></a>
+            <div className="bookDetails">
+              <div className="bookName">{book.bookName} </div>
+              <div className="bookAuthor">{book.author}</div>
+              <div className="bookRating"> {book.rating} </div>
+            </div>
+          </div>
+        ))}
+        </div>
+    </div>
+  )
+}
+export default App
---
+++ src/Booksdata.js
@@ -0,0 +1,74 @@
+export default {
+    PersonalGrowth:[
+      {
+        softcopy:"https://www.researchgate.net/profile/Rzger-Abdula/post/What_leadership_books_can_you_recommend_for_me/attachment/5bae7b2fcfe4a76455f6c7c2/AS%3A675912282550277%401538161454808/download/The+Magic+of+Thinking+Big.pdf",
+        bookName: "The Magic Of Thinking BIG",
+        author: "David J. Schwartz",
+        photoURL:
+          "https://m.media-amazon.com/images/I/814n2fhKdvL._AC_UY327_FMwebp_QL65_.jpg",
+        rating: "4.5/5"
+      },
+      {
+        softcopy:"https://librarykvemebrd.files.wordpress.com/2019/04/the-5-am-club.pdf",
+        bookName: "The 5 AM Club",
+        author: "Robin Sharma",
+        photoURL:
+          "https://images-na.ssl-images-amazon.com/images/I/71eijJe2ntL.jpg",
+        rating: "4/5"
+      }
+    ],
+    Finance: [
+      {
+        softcopy:"https://nooraljorany.com/book/buffet2.pdf",
+        bookName: "The Intelligent Investor",
+        author: "Benjamin Graham",
+        photoURL:
+          "https://images-na.ssl-images-amazon.com/images/I/91+t0Di07FL.jpg",
+        rating: "5/5"
+      },
+      {
+        softcopy:"https://manageo.io/wp-content/uploads/2018/08/Think-And-Grow-Rich.pdf",
+        bookName: "Think and Grow Rich",
+        author: "Napoleon Hill",
+        photoURL:
+          "https://images-na.ssl-images-amazon.com/images/I/91mJmoqx5zL.jpg",
+        rating: "5/5"
+      }
+    ],
+    Spirituality: [
+      {
+        softcopy:"https://selfdefinition.org/krishnamurti/Jiddu_Krishnamurt_Freedom_from_the_Known.pdf",
+        bookName: "Freedom From The Known",
+        author: "J. Krishnamurthy",
+        photoURL:
+          "https://m.media-amazon.com/images/I/71YJQwFT3GL._AC_UY327_FMwebp_QL65_.jpg",
+        rating: "5/5"
+      },
+      {
+        softcopy:"https://ati.dae.gov.in/ati12052021_6.pdf",
+        bookName: "The Power of Now",
+        author: "Eckhart Tolle",
+        photoURL:
+          "https://images-na.ssl-images-amazon.com/images/I/41cbPheOxmL._SX323_BO1,204,203,200_.jpg",
+        rating: "4.5/5"
+      }
+    ],
+    JavaScript: [
+      {
+        softcopy:"https://eloquentjavascript.net/Eloquent_JavaScript.pdf",
+        bookName: "Eloquent JavaScript",
+        author: "Marijn Haverbeke",
+        photoURL:
+          "https://images-na.ssl-images-amazon.com/images/I/91q8Jx+j6iL.jpg",
+        rating: "4.5/5"
+      },
+      {
+        softcopy:"https://pdfcoffee.com/qdownload/js-yet-get-started-by-kyle-simpson-simon-stlaurent-editorpdf-pdf-free.html",
+        bookName: "You Don't Know JS Yet",
+        author: "Kyle Simpson",
+        photoURL:
+          "https://images-na.ssl-images-amazon.com/images/I/71mKvD89oEL.jpg",
+        rating: "4.5/5"
+      }
+    ]
+  };
